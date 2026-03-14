7bacefd9396ca52e3880ca09bcc891ec0c0e5ab3
matheusmarnt <matheus.marnt@gmail.com>
2026-03-14 16:07:14 -0300

    feat: add en and pt_BR translation files for icon picker

---
+++ resources/lang/en/icon-picker.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'no_icon_selected'   => 'No icon selected',
+    'remove_icon'        => 'Remove icon',
+    'choose'             => 'Choose',
+    'choose_icon'        => 'Choose Icon',
+    'icon_libraries'     => 'Icon libraries',
+    'search_placeholder' => 'Search icon by name...',
+    'icons_count'        => ':count icons',
+    'filtered_by'        => 'filtered by ":term"',
+    'loading'            => 'Loading icons...',
+    'page_info'          => 'Page :current of :last',
+    'no_icons_found'     => 'No icons found',
+    'no_icons_hint'      => 'Try another term or select another library',
+    'cancel'             => 'Cancel',
+];
